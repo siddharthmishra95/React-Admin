3656c59f59bdc4a37aa6e7228e1861b733f2f842
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-18 19:46:28 +0530

    Added Chart

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 3000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 2000,
+    },
+    {
+        name: 'Apr',
+        "Active User": 1000,
+    },
+    {
+        name: 'May',
+        "Active User": 3000,
+    },
+    {
+        name: 'Jun',
+        "Active User": 4000,
+    },
+    {
+        name: 'Jul',
+        "Active User": 6000,
+    },
+    {
+        name: 'Aug',
+        "Active User": 8000,
+    },
+    {
+        name: 'Sep',
+        "Active User": 2000,
+    },
+    {
+        name: 'Oct',
+        "Active User": 5000,
+    },
+    {
+        name: 'Nov',
+        "Active User": 7000,
+    },
+    {
+        name: 'Dec',
+        "Active User": 8000,
+    },
+
+];
