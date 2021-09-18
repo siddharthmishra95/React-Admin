ab66b65d62ddf0de5bb3b8503412b4f5ac710111
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-18 22:15:21 +0530

    Added Product Page, ProductList and NewProduct

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import {
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 
 function App() {
@@ -30,6 +33,15 @@ function App() {
           </Route>
           <Route path="/newUser">
             <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
           </Route>
         </Switch>
       </div>
