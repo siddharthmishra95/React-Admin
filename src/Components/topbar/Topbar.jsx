65c95d13aa705c2a6d992e2ce11dcf2fd639b504
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-18 19:15:04 +0530

    Added Featured , Home ,Topbar & Sidebar

---
+++ src/Components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./topbar.css"
+import { Language, NotificationsNone, Settings } from '@material-ui/icons'
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">React Admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
